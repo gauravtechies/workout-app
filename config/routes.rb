55ffa2bc6c9833937f0c02e5646b40c4fda5b29e
Gaurav kamboj <gauravkamboj13121992@gmail.com>
2018-10-16 11:37:00 +0530

    push to git

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+
+Rails.application.routes.draw do
+  devise_for :users
+  get 'dashboards/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "dashboards#index"
+  resources :users do 
+    resources :exercises
+  end
+end
